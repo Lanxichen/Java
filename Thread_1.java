70c7899d433ead298d33bace98b24cee2329bda0
Lanxichen <1967925825@qq.com>
2019-01-10 15:07:06 +0800

    code commit test

--- Thread_1.java
+++ Thread_1.java
@@ -1,18 +1,18 @@
 package studywell;
 
-public class Thread_1 implements Runnable{
+public class Thread_1 implements Runnable{　//实现Runnable接口
 	public void run(){
 		for(;;) {
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(1000);  //线程休息一秒
 				
-			} catch (InterruptedException e) {}
+			} catch (InterruptedException e) {}　//捕获异常
 			System.out.println("Hello");
 		}
 	}
 	public static void main(String[] args) {
-		Thread_1 t=new Thread_1();
-		Thread thread=new Thread(t);
-		thread.start();
+		Thread_1 t=new Thread_1();　　//创建对象
+		Thread thread=new Thread(t);　//创建线程
+		thread.start();　　//启动线程
 	}
 }
